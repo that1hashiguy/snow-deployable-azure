203ef865880cd16c1c8c521c1aba668c99ec83b1
Carlos Hernandez <carlos.hernandez@carlos.hernandez-GFWY2JW4VJ>
2025-07-22 10:06:06 -0400

    init commit

---
+++ main.tf
@@ -0,0 +1,52 @@
+terraform {
+  required_providers {
+    azurerm = {
+      version = ">= 4.13.0"
+      source = "hashicorp/azurerm"
+    }
+  }
+
+  cloud {
+    organization = "hashi-fed-snow-demo-org"
+
+    workspaces {
+      name = "snow-deployable-azure"
+    }
+  }
+  required_version = ">= 1.5.6"
+}
+
+provider "azurerm" {
+  #alias = "azure-default"
+  features {}
+  #subscription_id = "ecf4703c-9033-4da0-b7dd-0f0c33df8dfe"
+}
+
+module "azure-modules" {
+  source  = "app.terraform.io/HashiDemoOrg_001/ilm/azure"
+  version = "1.0.15"
+
+  public_ip_prefix_id = var.pip_prefix
+  subnet_id = azurerm_subnet.main.id
+  suffix = "snow"
+  ssh-key = var.ssh-key
+}
+
+resource "azurerm_resource_group" "main" {
+ name     = "snow-demo-rg"
+ location = "eastus"
+}
+
+resource "azurerm_virtual_network" "main" {
+ address_space       = ["10.0.0.0/24"]
+ name = "snow-demo-vnet"
+ location            = azurerm_resource_group.main.location
+ resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "main" {
+  name = "snow-demo-snet"
+  resource_group_name = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes = [ cidrsubnet(azurerm_virtual_network.main.address_space[0], 4, 0) ]
+}
